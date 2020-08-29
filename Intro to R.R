5d6927ff16721588fbda98f429c8c1d2ad8c215f
Sean Werfel <scw224@lehigh.edu>
2020-08-29 16:13:14 -0400

    Edited Header

--- Intro to R.R
+++ Intro to R.R
@@ -1,8 +1,8 @@
 # R script to accompany Intro to R
-# Copy for sean-werfel 2019
+# Copy for sean-werfel Business Major 2019
 ######################################################
 
-# You will see that you can put non-executable "comment" lines
+# Copy for sean-werfel
 # in your script by putting "#" at the beginning of the lines.
 # 
 # In fact, you can put comments anywhere on a line as long as 
